f2286ca0be5015338e05601390e890b84c20aac8
AbsintheSung <h90243768@gmail.com>
2025-03-30 10:02:51 +0800

    feat:配置路由設定

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "normalize.css";
 import "@/assets/style/index.css"
-import App from './App.tsx'
+// import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom';
+import Router from './router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={Router}></RouterProvider>
   </StrictMode>,
 )
